70680031020854339191f200340dec38976de591
Kristaps Blumbergs <kristapsb52@gmail.com>
2022-01-06 22:12:08 +0200

    Fixed login, database issues

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, url_for
-
+from flask import (Blueprint, flash, g, redirect, render_template, request, url_for)
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
@@ -10,12 +9,15 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+    if db is not None:
+        posts = db.execute(
+            'SELECT p.id, title, body, created, author_id, username'
+            ' FROM post p JOIN user u ON p.author_id = u.id'
+            ' ORDER BY created DESC'
+        ).fetchall()
+        
+        return render_template('blog/index.html', posts=posts)
+    return render_template('blog/index.html')
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
@@ -32,13 +34,16 @@ def create():
             flash(error)
         else:
             db = get_db()
-            db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                'VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
-            )
-            db.commit()
-            return render_template('blog/create.html')
+            if db is not None:
+                db.execute(
+                    'INSERT INTO post (title, body, author_id)'
+                    'VALUES (?, ?, ?)',
+                    (title, body, g.user['id'])
+                )
+                db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
         
 def get_post(id, check_author=True):
     post = get_db().execute(
